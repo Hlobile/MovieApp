f213a7f8ac236352e64715ae2dfa94963d5775c9
Hlobile <62103682+Hlobile@users.noreply.github.com>
2020-12-15 08:42:30 +0200

    Add project files.

---
+++ MovieApp/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MovieApp.Models;
+
+namespace MovieApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MovieApp.Data.MovieAppContext _context;
+
+        public IndexModel(MovieApp.Data.MovieAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
